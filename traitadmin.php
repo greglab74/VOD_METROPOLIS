8f06df9a67f4eef3b8e43843e79ae715da936d13
greglab74 <39262246+greglab74@users.noreply.github.com>
2018-07-18 21:15:37 +0200

    g

--- traitadmin.php
+++ traitadmin.php
@@ -6,6 +6,8 @@ $annee = $_POST["annee"];
 $realisateur = $_POST["realisateur"];
 $acteurs= $_POST["acteurs"];
 $synopsis = $_POST["synopsis"];
+$affiche = $_POST["affiche"];
+$bandeannonce = $_POST["bandeannonce"];
 
 
 $test = $_POST["acteurs"];
@@ -23,17 +25,17 @@ try {
     $conn = new PDO("mysql:host=$serveur;dbname=$nom_bd", $login, $mot_de_passe);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql1 = "INSERT INTO films (nom, synopsis, annee, ID_administrateur)
-    VALUES ('$nom','$synopsis','$annee', '1')";
+    $sql1 = "INSERT INTO films (nom, synopsis, annee, affiche, bandeannonce)
+    VALUES ('$nom','$synopsis','$annee','$affiche','$bandeannonce')";
     $sql2 = "INSERT INTO genre (nom)
     VALUES ('$genre')";
     $sql3 = "INSERT INTO realisateurs (nom)
     VALUES ('$realisateur')";
     $sql4 = "INSERT INTO acteurs (nom)
     VALUES ('$aff[0]', '$aff[1]', '$aff[2]', '$aff[3]')";
-    INSERT INTO `joue` (`ID`, `ID_acteurs`) VALUES ('SELECT 'id' FROM 'films' WHERE name = '$nom';', 'SELECT id FROM acteurs WHERE name = '$aff[0]';'), ('13', '10'), ('13', '9'), ('13', '11');
-    INSERT INTO `A` (`ID`, `ID_genre`) VALUES ('SELECT id FROM films WHERE name = '$nom';', 'SELECT id FROM genre WHERE name = '$genre';');
-    INSERT INTO `realise` (`ID`, `ID_realisateurs`) VALUES ('SELECT id FROM films WHERE name = '$nom';', 'SELECT id FROM realisateurs WHERE name = '$realisateur';');
+    INSERT INTO `joue` (`ID_films`, `ID_acteurs`) VALUES ('SELECT ID_films FROM films WHERE name = "'.$nom.'";', 'SELECT ID_acteurs FROM acteurs WHERE name = '$aff[0]';'), ('13', '10'), ('13', '9'), ('13', '11');
+    INSERT INTO `A` (`ID_films`, `ID_genre`) VALUES ('SELECT ID_films FROM films WHERE name = "'.$nom.'";', 'SELECT ID_genre FROM genre WHERE name = "'.$genre.'";');
+    INSERT INTO `realise` (`ID`, `ID_realisateurs`) VALUES ('SELECT id FROM films WHERE name = "'.$nom.'";', 'SELECT ID_realisateurs FROM realisateurs WHERE name = "'.$realisateur.'";');
 
     // use exec() because no results are returned
     $conn->exec($sql1);
